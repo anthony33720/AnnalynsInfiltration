ca6adcf195d03b9a7320329e7a63bb4c4f2ee1ce
Anthony33720 <raynaud.anthony@gmail.com>
2024-02-13 09:44:42 +0100

    jhh

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,7 @@ class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
         throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFastAttack() method");
     }
-
-    public
+    gigig
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
         throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSpy() method");
